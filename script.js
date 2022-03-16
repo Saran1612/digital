92998ad7775c1b10e7fec0fe7a88dbac778b968e
Saran1612 <99238136+Saran1612@users.noreply.github.com>
2022-03-17 00:53:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const body = document.body;
+const nav = document.querySelector(".page-header nav");
+const menu = document.querySelector(".page-header .menu");
+const lottiePlayer = document.querySelector("lottie-player");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+ 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+ 
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+    lottiePlayer.play();
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains(scrollDown)
+  ) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+    lottiePlayer.stop();
+  }
+  lastScroll = currentScroll;
+});
